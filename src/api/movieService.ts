6ab0a89751903af216195843f5d9df66ad329e9b
craftomeCJ <javier.ho@activate.sg>
2025-01-26 01:18:07 +0800

    MOBILE-CODE-TEST][CJ][API][Update]: movieService to include type definitions and return types
    Adds type definitions for Movie and MovieDetails
    Updates fetchUpcomingMovies and fetchPopularMovies to include return type of Promise<Movie[]>
    Updates fetchMovieDetails to include return type of Promise<MovieDetails>

--- src/api/movieService.ts
+++ src/api/movieService.ts
@@ -1,20 +1,21 @@
 import axios from 'axios';
 import Config from 'react-native-config';
+import { Movie, MovieDetails } from '../types/type';
 
-const API_KEY = Config.TMDB_API_KEY;
+const API_KEY = Config.TMDB_API_KEY; // Access API key from .env
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export const fetchUpcomingMovies = async () => {
+export const fetchUpcomingMovies = async (): Promise<Movie[]> => {
   const response = await axios.get(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}`);
   return response.data.results;
 };
 
-export const fetchPopularMovies = async () => {
+export const fetchPopularMovies = async (): Promise<Movie[]> => {
   const response = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   return response.data.results;
 };
 
-export const fetchMovieDetails = async (id: number) => {
+export const fetchMovieDetails = async (id: number): Promise<MovieDetails> => {
   const response = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
   return response.data;
 };
